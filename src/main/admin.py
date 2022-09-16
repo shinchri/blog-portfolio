35e16ce2c916adfcd0e4f7e8b490ecf0858694e3
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-16 17:57:12 -0500

    feat: Create models and set admin; Create signals to create userprofile when user is created

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from . models import (
+    UserProfile,
+    ContactProfile,
+    Media,
+    Portfolio,
+    Blog,
+    Certificate,
+    Skill
+    )
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user')
+
+@admin.register(ContactProfile)
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ('id', 'timestamp', 'name',)
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('id','name','is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id','title','is_active')
+    readonly_fields = ('slug',)
+
+@admin.register(Certificate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id','name','score', 'is_key_skill')
