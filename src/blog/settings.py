f286975ac168cb9ea686435b381be8a9c5dd2f89
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-16 17:22:08 -0500

    feat: Set up static files and set urls to main

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 import environ
 from pathlib import Path
 
@@ -129,7 +130,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static'),
+  os.path.join(BASE_DIR, 'media')
+]
+
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
